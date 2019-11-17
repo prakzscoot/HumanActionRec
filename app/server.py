61212b91f4388b2733cd0dcb9297c86098c47738
prabhakar04 <35762012+prabhakar04@users.noreply.github.com>
2019-11-17 17:24:51 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=12JtqqM765-TwnbmFKYIKIl4MrhpYqf6T'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-AXIw5rVXutG0HEyuGzy-THIQNT50tDO'
 export_file_name = 'export.pkl'
 
-classes = ['Deep', 'Early', 'Moderate','Normal', 'OHT']
+classes = ['Applauding', 'BlowingBubbles', 'Brushing','Drinking']
 path = Path(__file__).parent
 
 app = Starlette()
